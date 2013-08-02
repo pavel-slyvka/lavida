2771189e2e4c572f5a45f7efd40a0d6dfdf6e058
Ruslan <fra.prsnl@gmail.com>
2013-08-02 14:41:19 +0300

    Story1. added assignAuthoritySave() and assignAuthorityUpdate() methods to UserService

--- finance/service/src/main/java/com/lavida/service/Main.java
+++ finance/service/src/main/java/com/lavida/service/Main.java
@@ -16,13 +16,15 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 public class Main {
     public static void main(String[] args) {
         UserJdo user3 = new UserJdo("login3","pass3", true, "name3");
-        user3.getAuthoritieses().add(new Authorities("ROLE_USER", user3));
+        Authorities authorities3 = new Authorities("ROLE_USER");
+        user3.getAuthoritieses().add(authorities3);
         String filePath ="spring-context.xml";
         AbstractApplicationContext context = new ClassPathXmlApplicationContext(filePath);
-        JpaTransactionManager transactionManager = context.getBean("transactionManager", JpaTransactionManager.class);
+//        JpaTransactionManager transactionManager = context.getBean("transactionManager", JpaTransactionManager.class);
         UserService service = context.getBean("userService", UserService.class);
-        service.save(user3);
+//        service.save(user3);
 //        service.update(user2);
+        service.assignAuthoritySave(user3, authorities3);
         System.out.println(service.getAll());
         System.out.println("Privet!");
     }
